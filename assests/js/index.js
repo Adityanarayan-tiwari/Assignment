6643b524aa775a9a99e0b41979555f20e69a04a2
Adityanarayan Tiwari <aditya17121997@gmail.com>
2020-07-25 16:31:14 +0530

    Task Completed

---
+++ assests/js/index.js
@@ -0,0 +1,70 @@
+
+// Javascript
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("btnactive");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" btnactive", "");
+  }
+  this.className += " btnactive";
+  });
+}
+
+
+
+
+
+
+
+
+//Jquery
+$(document).ready(function(){
+    $('.card-wrapper').slick({
+        
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    
+    accessibility: true,
+    adaptiveHeight: true,
+    centerMode: true,
+    centerPadding: '0px',
+
+    nextArrow:$('.next'),
+    prevArrow:$('.prev'),
+    responsive: [
+    {
+        breakpoint: 1240,
+        settings: {
+            arrows: true,
+            slidesToShow: 3
+        }
+        },
+        {
+        breakpoint: 768,
+        settings: {
+            arrows: false,
+            slidesToShow: 1
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            arrows: false,
+            slidesToShow: 1
+        }
+        }
+    ]
+    
+    });
+});
+
+
+
+
+
+
+
+
+
